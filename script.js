2f8b26b4bf36d2822aef7acd684794b503af588d
Bishal <bishalc878@gmail.com>
2025-08-17 12:51:07 +0100

    Bishal: Giving some functions

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       .scrollIntoView({ behavior: 'smooth' });
   });
 });
+
+
+function openReservation() {
+  document.getElementById("reservationModal").style.display = "block";
+}
+
+function closeReservation() {
+  document.getElementById("reservationModal").style.display = "none";
+}
+
+// Hidden form submission
+document.getElementById("reservationForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("✅ Your table has been reserved successfully!");
+  closeReservation();
+});
